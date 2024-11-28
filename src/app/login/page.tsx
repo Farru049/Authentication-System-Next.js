de7a852e1c1d11e2883b2ec29ae6a40e708a0150
Farhaan Ali <alifarhaan655@gmail.com>
2024-11-29 02:18:23 +0530

    build changes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -91,12 +91,10 @@ export default function LoginPage() {
                 </div>
                 <hr className="my-4" />
                 <div className="text-center">
-                    <Link
-                        href="/signup"
-                        className="text-blue-600 hover:text-blue-700 text-sm"
-                    >
-                        Don't have an account? Sign up
-                    </Link>
+                <Link href="/signup" className="text-blue-600 hover:text-blue-700 text-sm">
+  Don&apos;t have an account? Sign up
+                </Link>
+
                 </div>
             </div>
         </div>
