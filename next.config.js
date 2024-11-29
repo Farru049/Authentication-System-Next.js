e4bcdf42805b6c8f58419dc024b1a03642d55a9e
Farhaan Ali <alifarhaan655@gmail.com>
2024-11-29 17:39:20 +0530

    Configured next.config.js for standalone deployment and updated production settings

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: 'standalone', // Important for Vercel deployment
     typescript: {
       ignoreBuildErrors: true,
     },
