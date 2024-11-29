290388297b9568cc8875253e1c07a9bf7dc160a2
Farhaan Ali <alifarhaan655@gmail.com>
2024-11-29 17:08:12 +0530

    fixed the errors of build

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, // Enforce React best practices
+    reactStrictMode: true,
+    output: 'standalone', // Important for Vercel deployment
     typescript: {
-      ignoreBuildErrors: true, // Ignore TypeScript errors during build
+      ignoreBuildErrors: true,
     },
-    // If you use a custom output directory (optional):
-    // distDir: 'build',
     env: {
-      // Define any required environment variables here
       NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     },
-  };
-  
-  module.exports = nextConfig;
-  
+};
+
+module.exports = nextConfig;
